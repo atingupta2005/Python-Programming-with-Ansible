e766d73f384855a9da7051aa2d8cbf87a8c1f60c
Atin Gupta <atingupta@email.com>
2024-08-13 06:57:08 +0530

    -

--- Hands-on/03/03-Working-with-managed-nodes-using-raw-modules.md
+++ Hands-on/03/03-Working-with-managed-nodes-using-raw-modules.md
@@ -1,5 +1,4 @@
 # Working with mangaged nodes using raw module
-- Launch a new VM which do not have Python
 - Run raw module
   - ansible all -m raw -a "uptime"
   - 
--- Hands-on/03/simple-dynamic-inventory.py
+++ Hands-on/03/simple-dynamic-inventory.py
@@ -2,28 +2,20 @@
 import sys
 import json
 import pprint
-try:
-   import boto3
-  # print("Successfully imported boto3")
-except ImportError:
-   print("Please install boto3 using pip2 install boto3 or pip install boto3 and try again")
-   sys.exit(1)
-except Exception as e:
-   print(e)
-   sys.exit(2)
-def get_hosts(ec2,f_value):
-   f={"Name":"tag:Env", "Values":[f_value]}
+
+def get_hosts():
+   myhosts = ["10.0.0.10", "10.0.0.11"]
    hosts=[]
-   for each_in in ec2.instances.filter(Filters=[f]):
-    #  print(each_in.private_ip_address)
-    hosts.append(each_in.public_ip_address)
+   
+   for ip_address in myhosts:
+      hosts.append(ip_address)
+   
    return hosts
+
+
 def main():
-   ec2=boto3.resource("ec2","us-east-2")
-   db_group=get_hosts(ec2,"db")
-   app_group=get_hosts(ec2,"app")
+   db_group=get_hosts()
    all_groups= { 'db': db_group,
-                  'app': app_group
                }
    print(json.dumps(all_groups))
 
